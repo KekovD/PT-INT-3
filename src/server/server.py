ba06b5c7d9216402769d4ee8a487a62403fba9fb
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-06 16:19:19 +0300

    feat: add argparse in server.py

--- src/server/server.py
+++ src/server/server.py
@@ -1,25 +1,35 @@
-from src.server.tcp_handler.TCPHandler import TCPHandler
-from src.server.threaded_tcp_server.ThreadedTCPServer import ThreadedTCPServer
-from src.server.responsibility_chain.CheckLocalFileCommand import CheckLocalFileCommand
-from src.server.responsibility_chain.QuarantineLocalFileCommand import QuarantineLocalFileCommand
+import argparse
+
+from tcp_handler.TCPHandler import TCPHandler
+from threaded_tcp_server.ThreadedTCPServer import ThreadedTCPServer
+from responsibility_chain.CheckLocalFileCommand import CheckLocalFileCommand
+from responsibility_chain.QuarantineLocalFileCommand import QuarantineLocalFileCommand
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Start the TCP Server with specified parameters.")
+    parser.add_argument('HOST', type=str, help='Hostname or IP address to bind the server to.')
+    parser.add_argument('PORT', type=int, help='Port number to bind the server to.')
+    parser.add_argument('MAX_THREADS', type=int, help='Maximum number of threads for the server.')
+    parser.add_argument('QUARANTINE_PATH', type=str, help='Path to the quarantine directory.')
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 1414
-    MAX_THREADS = 10
+    args = parse_arguments()
 
-    server_address = (HOST, PORT)
+    server_address = (args.HOST, args.PORT)
+    quarantine_path = args.QUARANTINE_PATH
 
     command_chain = CheckLocalFileCommand()
     quarantine_command = QuarantineLocalFileCommand()
     command_chain.add_next(quarantine_command)
 
-    quarantine_path = '/home/kekov/Documents/239'
-
     server = ThreadedTCPServer(
         server_address,
         TCPHandler,
-        MAX_THREADS,
+        args.MAX_THREADS,
         quarantine_path,
         command_chain)
 
