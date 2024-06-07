a123c0382a4a5ca55245277da3c54c86ebc4586d
Dmitry Lavrenov <lavrenov-2015@bk.ru>
2024-06-07 17:10:02 +0300

    test: refactor test_invalid_command_and_params_sent.py

--- tests/test_invalid_command_and_params_sent.py
+++ tests/test_invalid_command_and_params_sent.py
@@ -1,4 +1,3 @@
-import os
 import socket
 import pytest
 
@@ -12,52 +11,76 @@ def send_and_receive(host: str, port: str, data: str) -> str:
 
 
 @pytest.mark.parametrize("start_server", [{"threads": 10}], indirect=True)
-def test_invalid_command_and_params_sent(start_server, start_client, host, port):
-    large_signature = ""
-    while len(large_signature) < 1100:
-        large_signature += "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-    large_request = large_signature + large_signature
-
-    client_params_list = [
-        {"command": "example_command", "params": {"param1": "value1"}},
-        {"command": "QuarantineLocalFile", "params": {"param": "value"}},
-        {"command": "QuarantineLocalFile", "params": {"param1": ""}},
-        {"command": "CheckLocalFile", "params": {"param1": "value", "param2": ""}},
-        {"command": "CheckLocalFile", "params": {"param1": "", "param2": ""}},
-        {"command": "CheckLocalFile", "params": {"param1": "value", "param2": large_signature}},
-        {"command": "CheckLocalFile", "params": {"param1": "value", "param2": large_request}},
-    ]
-
-    error_msgs = [
-        "Received: Unknown command",
-        "Received: Invalid query format. Parameters missing: param1, Extra parameters provided: param",
-        "Received: Invalid query format. Local file path cannot be empty in param1",
-        "Received: Invalid query format. Parameters cannot be empty: signature - param2",
-        "Received: Invalid query format. Parameters cannot be empty: local file path - param1, signature - param2",
-        "Received: Invalid query format. Signature is too large",
-        "Received: Message exceeds 2048 bytes. Request too large",
-    ]
-
-    for params, error_msg in zip(client_params_list, error_msgs):
-        client_output = start_client(params)
-        assert error_msg in client_output
-
-    test_cases = [
+@pytest.mark.parametrize(
+    "client_params, expected_error_msg, data, expected_result",
+    [
         (
-            '''{"command": "CheckLocalFile", "temp": {"param1": "Path"}}''',
+            {"command": "example_command", "params": {"param1": "value1"}},
+            "Received: Unknown command",
+            None,
+            None
+        ),
+        (
+            {"command": "QuarantineLocalFile", "params": {"param": "value"}},
+            "Received: Invalid query format. Parameters missing: param1, Extra parameters provided: param",
+            None,
+            None
+        ),
+        (
+            {"command": "QuarantineLocalFile", "params": {"param1": ""}},
+            "Received: Invalid query format. Local file path cannot be empty in param1",
+            None,
+            None
+        ),
+        (
+            {"command": "CheckLocalFile", "params": {"param1": "value", "param2": ""}},
+            "Received: Invalid query format. Parameters cannot be empty: signature - param2",
+            None,
+            None
+        ),
+        (
+            {"command": "CheckLocalFile", "params": {"param1": "", "param2": ""}},
+            "Received: Invalid query format. Parameters cannot be empty: local file path - param1, signature - param2",
+            None,
+            None
+        ),
+        (
+            {"command": "CheckLocalFile", "params": {"param1": "value", "param2": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" * 22}},
+            "Received: Invalid query format. Signature is too large",
+            None,
+            None
+        ),
+        (
+            {"command": "CheckLocalFile", "params": {"param1": "value", "param2": "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" * 44}},
+            "Received: Message exceeds 2048 bytes. Request too large",
+            None,
+            None
+        ),
+        (
+            None,
+            None,
+            '{"command": "CheckLocalFile", "temp": {"param1": "Path"}}',
             "Invalid query format. Missing keys: params. Invalid keys: temp"
         ),
         (
+            None,
+            None,
             "temp",
             "Invalid JSON format. Request must be in JSON format."
         ),
         (
+            None,
+            None,
             "{ }",
             "Request is empty"
-        )
+        ),
     ]
+)
+def test_invalid_command_and_params_sent(start_server, start_client, host, port, client_params, expected_error_msg, data, expected_result):
+    if client_params:
+        client_output = start_client(client_params)
+        assert expected_error_msg in client_output
 
-    for data, expected_result in test_cases:
+    if data:
         received = send_and_receive(host, port, data)
         assert received == expected_result
